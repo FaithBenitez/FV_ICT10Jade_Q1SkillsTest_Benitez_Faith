73b9189450f51fafde59e04ce032e0379c867c18
benitez-faith <benitez.fo@obmontessori.edu.ph>
2026-09-15 21:47:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
 from pyscript import document, display
 
-def add_num(e): #put e for the event handler
-    document.getElementById("output1").innerHTML = " " # clears previous result
-
-    num1 = float(document.getElementById("input1").value) #get input value
-    num2 = float(document.getElementById("input2").value) #get input value
-    result = num1 + num2 #use of addition operator
-    display(result, target="output1") #display result
-
 def create_order(e):
-    document.getElementById("output2").innerHTML = " " # clears previous result
+    document.getElementById("output2").innerHTML = " "  # clears previous result
 
-    prod1 = document.getElementById("item1") #get item 1 id
-
-    #Calculate
-    subtotal = float(prod1.value) * prod1.checked
-    size = document.querySelector("input[name='size']:checked")
-    price = float(size.value)
-    grandtotal = subtotal + price 
-
-    display(grandtotal, target="output2")
-
-def place_order(e):
-    document.getElementById("output3").innerHTML = " " # clears previous result
-    
-    coffee = document.getElementById("coffee")
-    coffee_price = float(coffee.value)
-    display(coffee_price, target = "output3")
-
-def show_order(e):
-    document.getElementById("output4").innerHTML = "" # clears previous result
-    prod1=document.getElementById("item1") #get item 1 id
-    subtotal= float(prod1.value) * prod1.checked
-    size = document.querySelector("input[name='size']:checked")
-    price = float(size.value)
-    grandtotal = subtotal + price
-    coffee = document.getElementById("coffee")
-    coffee_price = float(coffee.value)
-    final_order = grandtotal + coffee_price
-    display(f'You have to pay a total of {final_order}', target="output4")
+    # Get each item element
+    prod1 = document.getElementById("item1")
+    prod2 = document.getElementById("item2")
+    prod3 = document.getElementById("item3")
+    prod4 = document.getElementById("item4")
+    prod5 = document.getElementById("item5")
  
+
+    pastry = document.getElementById("Pastry")
+    pastry_price = float(pastry.value)
+
+    # Calculate subtotal 
+    subtotal = (float(prod1.value) * prod1.checked
+                + float(prod2.value) * prod2.checked
+                + float(prod3.value) * prod3.checked
+                + float(prod4.value) * prod4.checked
+                + float(prod5.value) * prod5.checked)       
+
+    
+    size = document.querySelector("input[name='size']:checked")
+    size_price = float(size.value)
+
+    # Tax and price with tax
+    tax_rate = 0.12  
+    tax = subtotal * tax_rate
+    
+    # Grand total
+    total = subtotal + size_price + tax + pastry_price
+
+    display(f"Total: {total}.", target="output1")
+    display(f"Subtotal: {subtotal}.", target="output1")
+    display(f"Tax: {tax}.", target="output1", append=True)
+    display(f"Thank you for your order!", target="output1", append=True)
